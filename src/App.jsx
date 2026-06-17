a39c0e83aabadf62c53131e63aa1367ba8dcfba5
Kevin Amorim <kevinamorim@f3dbe8b1-52d0-4901-bb4c-cac1d54ec82c.home>
2026-06-17 14:29:26 +0200

    add recruteur routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import RegisterRecruteur from './pages/RegisterRecruteur'
 import DashboardJoueur from './pages/DashboardJoueur'
 import DashboardCoach from './pages/DashboardCoach'
+import DashboardClub from './pages/DashboardClub'
 import Upload from './pages/Upload'
 import Feed from './pages/Feed'
 import UploadClip from './pages/UploadClip'
@@ -14,8 +16,10 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/register-recruteur" element={<RegisterRecruteur />} />
       <Route path="/dashboard" element={<DashboardJoueur />} />
       <Route path="/coach" element={<DashboardCoach />} />
+      <Route path="/club" element={<DashboardClub />} />
       <Route path="/upload" element={<Upload />} />
       <Route path="/feed" element={<Feed />} />
       <Route path="/upload-clip" element={<UploadClip />} />
